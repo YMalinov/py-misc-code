3b684509be89fa533f188e3113ac80dd7f9ccb53
Yanko Malinov <hafrox@gmail.com>
2013-06-20 01:03:47 +0300

    Update prime_checker.py

--- prime_checker.py
+++ prime_checker.py
@@ -3,7 +3,6 @@ import math
 def primeCheck(number):
  isPrime = False
  for i in range(2, int(math.sqrt(number))):
-   print str(i)
   if (inp % i == 0):
    isPrime = True
    break
@@ -14,3 +13,4 @@ inp = int(raw_input('Check prime number: '))
 print "Is prime? " + str(primeCheck(inp))
 
 # a somewhat efficient primer checker (checks if a number is prime or not)
+# works in O(sqrt(n)) steps
